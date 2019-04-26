2b3f5923ecf21bf40a1f0efd1737da40effc52bc
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2019-04-25 23:53:20 -0400

    Update module

--- includes/ClicShopping/Apps/Orders/Orders/Module/ClicShoppingAdmin/Dashboard/SalesCommission.php
+++ includes/ClicShopping/Apps/Orders/Orders/Module/ClicShoppingAdmin/Dashboard/SalesCommission.php
@@ -74,16 +74,23 @@
         $js_array = substr($js_array, 0, -1);
       }
 
-      $chart_label = HTML::output($this->app->getDef('module_admin_dashboard_sales_commission_app_chart_link'));
       $chart_label_link = '';
       $chart_title = HTML::output($this->app->getDef('module_admin_dashboard_sales_commission_app_chart_link'));
 
-      $content_width = 'col-lg-' . (int)MODULE_ADMIN_DASHBOARD_SALES_COMMISSION_APP_CONTENT_WIDTH;
+      $content_width = 'col-md-' . (int)MODULE_ADMIN_DASHBOARD_SALES_COMMISSION_APP_CONTENT_WIDTH;
 
       $output = <<<EOD
-<span class="$content_width text-md-center">
-<div class="text-md-center">$chart_title</div>
-<div id="d_sales_commission" class="col-md-12" style="width:100%; height: 200px;"></div>
+<div class="{$content_width}">
+  <div class="card-deck mb-3">
+    <div class="card">
+      <div class="card-body">
+        <h6 class="card-title"><i class="fa fa-coins"></i> {$chart_title}</h6>
+        <p class="card-text"><div id="d_sales_commission" class="col-md-12" style="width:100%; height: 200px;"></div></p>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script type="text/javascript">
 $(function () {
   var plot30 = [$js_array];
@@ -162,7 +169,6 @@ $('#d_sales_commission').bind('plothover', function (event, pos, item) {
 
 
 </script>
-</span>
 EOD;
 
       return $output;
@@ -188,85 +194,43 @@ EOD;
     }
 
     public function Install() {
-
       $this->installDbSalesCommission();
 
-      if ($this->lang->getId() != 2) {
+      $this->app->db->save('configuration', [
+          'configuration_title' => 'Do you want to enable this Module ?',
+          'configuration_key' => 'MODULE_ADMIN_DASHBOARD_SALES_COMMISSION_APP_STATUS',
+          'configuration_value' => 'True',
+          'configuration_description' => 'Do you want to enable this Module ?',
+          'configuration_group_id' => '6',
+          'sort_order' => '1',
+          'set_function' => 'clic_cfg_set_boolean_value(array(\'True\', \'False\'))',
+          'date_added' => 'now()'
+        ]
+      );
 
-        $this->app->db->save('configuration', [
-            'configuration_title' => 'Souhaitez vous activer ce module ?',
-            'configuration_key' => 'MODULE_ADMIN_DASHBOARD_SALES_COMMISSION_APP_STATUS',
-            'configuration_value' => 'True',
-            'configuration_description' => 'Souhaitez vous activer ce module ?',
-            'configuration_group_id' => '6',
-            'sort_order' => '1',
-            'set_function' => 'clic_cfg_set_boolean_value(array(\'True\', \'False\'))',
-            'date_added' => 'now()'
-          ]
-        );
+       $this->app->db->save('configuration', [
+          'configuration_title' => 'Select the width to display',
+          'configuration_key' => 'MODULE_ADMIN_DASHBOARD_SALES_COMMISSION_APP_CONTENT_WIDTH',
+          'configuration_value' => '12',
+          'configuration_description' => 'Select a number between 1 to 12',
+          'configuration_group_id' => '6',
+          'sort_order' => '1',
+          'set_function' => 'clic_cfg_set_content_module_width_pull_down',
+          'date_added' => 'now()'
+        ]
+      );
 
-        $this->app->db->save('configuration', [
-            'configuration_title' => 'Veuillez selectionner la largeur de l\'affichage?',
-            'configuration_key' => 'MODULE_ADMIN_DASHBOARD_SALES_COMMISSION_APP_CONTENT_WIDTH',
-            'configuration_value' => '6',
-            'configuration_description' => 'Veuillez indiquer un nombre compris entre 1 et 12',
-            'configuration_group_id' => '6',
-            'sort_order' => '1',
-            'set_function' => 'clic_cfg_set_content_module_width_pull_down',
-            'date_added' => 'now()'
-          ]
-        );
-
-        $this->app->db->save('configuration', [
-            'configuration_title' => 'Ordre de tri d\'affichage',
-            'configuration_key' => 'MODULE_ADMIN_DASHBOARD_SALES_COMMISSION_APP_SORT_ORDER',
-            'configuration_value' => '30',
-            'configuration_description' => 'Ordre de tri pour l\'affichage (Le plus petit nombre est montré en premier)',
-            'configuration_group_id' => '6',
-            'sort_order' => '99',
-            'set_function' => '',
-            'date_added' => 'now()'
-          ]
-        );
-
-      }else {
-
-        $this->app->db->save('configuration', [
-            'configuration_title' => 'Do you want to enable this Module ?',
-            'configuration_key' => 'MODULE_ADMIN_DASHBOARD_SALES_COMMISSION_APP_STATUS',
-            'configuration_value' => 'True',
-            'configuration_description' => 'Do you want to enable this Module ?',
-            'configuration_group_id' => '6',
-            'sort_order' => '1',
-            'set_function' => 'clic_cfg_set_boolean_value(array(\'True\', \'False\'))',
-            'date_added' => 'now()'
-          ]
-        );
-
-         $this->app->db->save('configuration', [
-            'configuration_title' => 'Select the width to display',
-            'configuration_key' => 'MODULE_ADMIN_DASHBOARD_SALES_COMMISSION_APP_CONTENT_WIDTH',
-            'configuration_value' => '12',
-            'configuration_description' => 'Select a number between 1 to 12',
-            'configuration_group_id' => '6',
-            'sort_order' => '1',
-            'set_function' => 'clic_cfg_set_content_module_width_pull_down',
-            'date_added' => 'now()'
-          ]
-        );
-
-         $this->app->db->save('configuration', [
-            'configuration_title' => 'Sort Order',
-            'configuration_key' => 'MODULE_ADMIN_DASHBOARD_SALES_COMMISSION_APP_SORT_ORDER',
-            'configuration_value' => '50',
-            'configuration_description' => 'Sort order of display. Lowest is displayed first.',
-            'configuration_group_id' => '6',
-            'sort_order' => '2',
-            'set_function' => '',
-            'date_added' => 'now()'
-          ]
-        );
-      }
+       $this->app->db->save('configuration', [
+          'configuration_title' => 'Sort Order',
+          'configuration_key' => 'MODULE_ADMIN_DASHBOARD_SALES_COMMISSION_APP_SORT_ORDER',
+          'configuration_value' => '50',
+          'configuration_description' => 'Sort order of display. Lowest is displayed first.',
+          'configuration_group_id' => '6',
+          'sort_order' => '2',
+          'set_function' => '',
+          'date_added' => 'now()'
+        ]
+      );
     }
 
     public function keys() {
